2b10ad421e38aa53bb4ba48db6d0b3bfdb306295
an-lee <an.lee.work@gmail.com>
2020-10-22 15:40:26 +0800

    fix(config): compile in deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -89,6 +89,7 @@ task :deploy do
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
     invoke :'rails:db_migrate'
+    invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
