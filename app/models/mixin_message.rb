33bb6a58c91615991ae20d3acf339ccda26cb103
an-lee <an.lee.work@gmail.com>
2020-10-27 18:16:49 +0800

    feat(mixin_message): process message

--- app/models/mixin_message.rb
+++ app/models/mixin_message.rb
@@ -22,4 +22,77 @@
 #  index_mixin_messages_on_message_id  (message_id) UNIQUE
 #
 class MixinMessage < ApplicationRecord
+  belongs_to :user, primary_key: :mixin_uuid, optional: true
+
+  before_validation :setup_attributes
+
+  after_commit :process_async, on: :create
+
+  scope :unprocessed, -> { where(processed_at: nil) }
+
+  def processed?
+    processed_at?
+  end
+
+  def process!
+    if category == 'SYSTEM_ACCOUNT_SNAPSHOT'
+      process_snapshot
+    else
+      send_default_reply
+    end
+
+    update processed_at: Time.current
+  end
+
+  # memo = {
+  #  t: BUY|REWARD,
+  #  a: article's uuid,
+  # }
+  def process_snapshot
+    snapshot =
+      begin
+        JSON.parse(content)
+      rescue JSON::ParserError
+        nil
+      end
+    return if snapshot.blank?
+
+    memo =
+      begin
+        JSON.parse Base64.decode64(snapshot['memo'])
+      rescue JSON::ParserError
+        {}
+      end
+
+    case memo['type']
+    when 'BUY'
+      # TODO: create buy article order
+    when 'REWARD'
+      # TODO: create reward article order
+    end
+  end
+
+  def process_async
+    # TODO: sidekiq worker
+  end
+
+  def send_default_reply
+    return if conversation_id.blank?
+    # TODO: send default reply via http
+  end
+
+  private
+
+  def set_attributes
+    return unless new_record?
+
+    data = raw['data']
+
+    self.action            = raw['action']
+    self.message_id        = data['message_id']
+    self.category          = data['category']
+    self.conversation_id   = data['conversation_id']
+    self.user_id           = data['user_id']
+    self.content           = Base64.decode64 data['data']
+  end
 end
