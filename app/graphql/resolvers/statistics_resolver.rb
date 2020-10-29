409c33ce2774181825f86d87886a81c419969db8
an-lee <an.lee.work@gmail.com>
2020-10-30 07:06:36 +0800

    fix(statistic): fix reader revenue

--- app/graphql/resolvers/statistics_resolver.rb
+++ app/graphql/resolvers/statistics_resolver.rb
@@ -9,7 +9,7 @@ module Resolvers
         users_count: User.count,
         articles_count: Article.count,
         author_revenue_amount: Transfer.where(transfer_type: :author_revenue).sum(:amount),
-        reader_revenue_amount: Transfer.where(transfer_type: :author_revenue).sum(:amount)
+        reader_revenue_amount: Transfer.where(transfer_type: :reader_revenue).sum(:amount)
       }
     end
   end
