c8925454ed8692a3926c77fa031f437476641c5b
an-lee <an.lee.work@gmail.com>
2020-10-31 06:03:55 +0800

    fix(view): handle csrf token nil

--- app/javascript/src/shared/apollo-client/apollo-client.tsx
+++ app/javascript/src/shared/apollo-client/apollo-client.tsx
@@ -1,12 +1,16 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-export const apolloClient = (uri: string, csrfToken: string) =>
-  new ApolloClient({
+export const apolloClient = (uri: string, csrfToken?: string) => {
+  return new ApolloClient({
     uri,
     cache: new InMemoryCache(),
     credentials: 'same-origin',
     headers: {
       'Content-Type': 'application/json',
-      'X-CSRF-Token': csrfToken,
+      'X-CSRF-Token':
+        csrfToken ||
+        ((document.querySelector("meta[name='csrf-token']") as any) || {})
+          .content,
     },
   });
+};
