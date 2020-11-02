05d6ba12696fbfc07f7c13581af125432268767c
an-lee <an.lee.work@gmail.com>
2020-11-03 06:13:47 +0800

    feat(view): add not found page

--- app/javascript/src/apps/application/Routes.tsx
+++ app/javascript/src/apps/application/Routes.tsx
@@ -5,31 +5,35 @@ import ArticlePage from './pages/ArticlePage/ArticlePage';
 import FairPage from './pages/FairPage/FairPage';
 import HomePage from './pages/HomePage/HomePage';
 import MinePage from './pages/MinePage/MinePage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import RulesPage from './pages/RulesPage/RulesPage';
 
 export default function Routes() {
   return (
     <React.Fragment>
-      <Route path='/' exact>
-        <HomePage />
-      </Route>
       <Switch>
+        <Route path='/' exact>
+          <HomePage />
+        </Route>
         <Route path='/articles/new' exact>
           <ArticleNewPage />
         </Route>
         <Route path='/articles/:uuid' exact>
           <ArticlePage />
         </Route>
+        <Route path='/rules' exact>
+          <RulesPage />
+        </Route>
+        <Route path='/mine' exact>
+          <MinePage />
+        </Route>
+        <Route path='/fair' exact>
+          <FairPage />
+        </Route>
+        <Route>
+          <NotFoundPage />
+        </Route>
       </Switch>
-      <Route path='/rules' exact>
-        <RulesPage />
-      </Route>
-      <Route path='/mine' exact>
-        <MinePage />
-      </Route>
-      <Route path='/fair' exact>
-        <FairPage />
-      </Route>
     </React.Fragment>
   );
 }
