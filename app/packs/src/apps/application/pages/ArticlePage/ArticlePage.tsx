45f083d40cb61db1f2547f76a844e8d6425b0ecf
an-lee <an.lee.work@gmail.com>
2021-02-22 19:03:51 +0800

    fix: use grid to display avatars

--- app/packs/src/apps/application/pages/ArticlePage/ArticlePage.tsx
+++ app/packs/src/apps/application/pages/ArticlePage/ArticlePage.tsx
@@ -334,7 +334,7 @@ export default function ArticlePage() {
             </Col>
           </Row>
           <div className='px-4 mb-4'>
-            <div className='flex flex-wrap justify-center m-auto w-60 md:w-96'>
+            <div className='m-auto grid grid-cols-8 w-72'>
               {article.randomReaders.map((reader: Partial<User>) => (
                 <Avatar
                   className='m-0.5'
