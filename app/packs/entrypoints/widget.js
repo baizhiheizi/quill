b8e3c4e0cce7be88f4340e6bab76494e8aa3b4d6
an-lee <an.lee.work@gmail.com>
2021-02-22 17:17:36 +0800

    fix: fix mark.js

--- app/packs/entrypoints/widget.js
+++ app/packs/entrypoints/widget.js
@@ -7,8 +7,8 @@ import 'stylesheets/widget.css';
 import Mark from 'mark.js';
 
 document.addEventListener('turbolinks:load', function () {
-  const context = document.querySelector('.articles-list');
+  const context = document.querySelector('.mark-context');
   const instance = new Mark(context);
-  const keywords = JSON.parse(document.querySelector('.query').dataset.query);
+  const keywords = JSON.parse(document.querySelector('.mark-keywords')?.dataset?.keywords);
   instance.mark(keywords);
 });
