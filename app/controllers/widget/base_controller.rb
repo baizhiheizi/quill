6cf4378df37224d8e256bdb7cbea59a76494736c
an-lee <an.lee.work@gmail.com>
2021-02-23 10:56:49 +0800

    feat(widget): set X-Frame-Options: ALLOWALL

--- app/controllers/widget/base_controller.rb
+++ app/controllers/widget/base_controller.rb
@@ -3,5 +3,13 @@
 class Widget::BaseController < ApplicationController
   include Pagy::Backend
 
+  before_action :set_x_frame_options
+
   layout 'widget'
+
+  private
+
+  def set_x_frame_options
+    response.headers['X-Frame-Options'] = 'ALLOWALL'
+  end
 end
