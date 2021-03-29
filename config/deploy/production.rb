4630aeae4c83e741a304da8940d3dd4d25b813b5
an-lee <an.lee.work@gmail.com>
2021-03-29 14:17:49 +0800

    feat: setup for cloudflare cdn

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :domain, 'prsdigg.com'
+set :domain, '47.244.21.224'
 set :deploy_to, '/data/www/prsdigg'
 set :repository, 'git@github.com:baizhiheizi/prsdigg.git'
 set :branch, 'build'
