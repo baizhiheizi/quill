9f2e91a003a0d11c7e7ca03be1a4f7e3c7520b77
an-lee <an.lee.work@gmail.com>
2021-05-13 09:39:55 +0800

    fix(sidekiq): remove WARNING

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@
 require 'sidekiq/web'
 require 'sidekiq/cron/web'
 
-Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_key_base]
-
 class AdminConstraint
   def matches?(request)
     return false if request.session[:current_admin_id].blank?
