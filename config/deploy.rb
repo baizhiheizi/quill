bea1adb75c160df779438e2605bee7dd5c3cdd8c
an-lee <an.lee.work@gmail.com>
2021-05-16 07:01:24 +0800

    fix(deploy): cp credentials file

--- config/deploy.rb
+++ config/deploy.rb
@@ -80,7 +80,8 @@ end
 
 desc 'link credentials file to for current stage'
 task :link_credentials_file do
-  command %(ln -sf "config/credentials.yml.enc" "config/credentials/#{fetch(:stage)}.yml.enc")
+  command %(echo "-----> exec: cp ./config/credentials/#{fetch(:stage)}.yml.enc ./config/credentials.yml.enc")
+  command %(cp "./config/credentials/#{fetch(:stage)}.yml.enc" "./config/credentials.yml.enc")
 end
 
 desc 'Deploys the current version to the server.'
