f2537793ba8daf2b6df2c569ee55aee5146e2b05
an-lee <an.lee.work@gmail.com>
2021-06-14 07:06:07 +0800

    feat(view): landing page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class SessionsController < ApplicationController
+  skip_before_action :ensure_launched!
   skip_before_action :verify_authenticity_token, only: :create
 
   def new
