b80176c38f675b283abee5c9c6ea0b82cc7d5cf0
an-lee <an.lee.work@gmail.com>
2021-06-19 13:19:56 +0800

    chore: deploy cmd

--- config/deploy/main.rb
+++ config/deploy/main.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-set :domain, '47.244.21.224'
+set :domain, 'prsdigg.com'
 set :deploy_to, '/data/www/prsdigg'
 set :repository, 'git@github.com:baizhiheizi/prsdigg.git'
 set :branch, 'master'
