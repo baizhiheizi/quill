48e9e595e55f63fc31f8fd4a334f6acb5640851a
an-lee <an.lee.work@gmail.com>
2021-06-30 16:55:49 +0800

    chore: fix render build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,5 +4,6 @@ set -o errexit
 
 cp config/credentials/main.yml.enc config/credentials.yml.enc
 cp config/settings.main.example.yml config/settings.yml
+yarn install
 bundle install
 bundle exec rake db:migrate
