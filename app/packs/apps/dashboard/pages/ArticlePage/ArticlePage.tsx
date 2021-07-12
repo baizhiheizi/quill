d84321fc865e9ad9bbec6d6bf9eb0cbe719fdbb3
an-lee <an.lee.work@gmail.com>
2021-07-12 08:55:40 +0800

    feat(article): can search to add reference

--- app/packs/apps/dashboard/pages/ArticlePage/ArticlePage.tsx
+++ app/packs/apps/dashboard/pages/ArticlePage/ArticlePage.tsx
@@ -58,7 +58,7 @@ export default function ArticlePage() {
           size='small'
           column={{ xxl: 4, xl: 3, lg: 3, md: 3, sm: 2, xs: 1 }}
         >
-          <Descriptions.Item label={t('article.stateText')}>
+          <Descriptions.Item label={t('article.state_text')}>
             <Tag
               color={
                 article.state === 'published'
