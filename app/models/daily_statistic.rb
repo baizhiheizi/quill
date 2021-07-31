f0fcd7c409844ecff7e2e1823e4df74434c586bf
an-lee <an.lee.work@gmail.com>
2021-07-31 10:25:12 +0800

    fix(statistic): fix daily statistic

--- app/models/daily_statistic.rb
+++ app/models/daily_statistic.rb
@@ -24,13 +24,23 @@ class DailyStatistic < Statistic
 
   def self.generate(date: Time.current.yesterday)
     create_with(
-      new_users_count: User.where(created_at: date.beginning_of_day...date.end_of_day).count,
-      paid_users_count: Payment.completed.where(created_at: ...date.end_of_day).pluck(:opponent_id).uniq.count,
-      new_payments_count: Payment.completed.where(created_at: date.beginning_of_day...date.end_of_day).count,
-      new_payers_count: Payment.completed.where(created_at: date.beginning_of_day...date.end_of_day).pluck(:opponent_id).uniq.count,
-      new_articles_count: Article.where(published_at: date.beginning_of_day...date.end_of_day).count
+      data_attributes(date)
     ).find_or_create_by(
       datetime: date
     )
+  end
+
+  def regenerate
+    update data_attributes(datetime)
+  end
+
+  def data_attributes(date)
+    {
+      new_users_count: User.where(created_at: date.beginning_of_day...date.end_of_day).count,
+      paid_users_count: Order.completed.where(order_type: %i[buy_article reward_article], created_at: ...date.end_of_day).pluck(:buyer_id).uniq.count,
+      new_payments_count: Order.completed.where(order_type: %i[buy_article reward_article], created_at: date.beginning_of_day...date.end_of_day).count,
+      new_payers_count: Order.completed.where(order_type: %i[buy_article reward_article], created_at: date.beginning_of_day...date.end_of_day).pluck(:buyer_id).uniq.count,
+      new_articles_count: Article.where(published_at: date.beginning_of_day...date.end_of_day).count
+    }
   end
 end
