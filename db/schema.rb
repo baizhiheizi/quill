6528f9c94d45fabaf98bdde005f1d9a4eb7b1ef0
an-lee <an.lee.work@gmail.com>
2021-09-14 11:46:52 +0800

    fix(user): mixin id not unique(prepare for fennec user)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_13_122313) do
+ActiveRecord::Schema.define(version: 2021_09_14_034505) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -443,7 +443,7 @@ ActiveRecord::Schema.define(version: 2021_09_13_122313) do
     t.integer "subscribers_count", default: 0
     t.integer "subscribing_count", default: 0
     t.string "uid"
-    t.index ["mixin_id"], name: "index_users_on_mixin_id", unique: true
+    t.index ["mixin_id"], name: "index_users_on_mixin_id"
     t.index ["mixin_uuid"], name: "index_users_on_mixin_uuid", unique: true
     t.index ["statistics"], name: "index_users_on_statistics", using: :gin
     t.index ["uid"], name: "index_users_on_uid", unique: true
