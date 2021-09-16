eaf51279384b00c1fa2b21c680fc98939240838f
an-lee <an.lee.work@gmail.com>
2021-09-16 09:07:20 +0800

    chore: use esbuild loader

--- config/webpack/custom.js
+++ config/webpack/custom.js
@@ -4,6 +4,14 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.tsx?$/,
+        loader: 'esbuild-loader',
+        options: {
+          loader: 'tsx', // Or 'ts' if you don't need tsx
+          target: 'es2015',
+        },
+      },
+      {
         test: /\.(less)$/i,
         use: [
           {
