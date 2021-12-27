71a991249d38f6d06c0add8cb599e28db4b1100c
an-lee <an.lee.work@gmail.com>
2021-12-27 21:36:29 +0800

    improve SEO

--- app/controllers/users/base_controller.rb
+++ app/controllers/users/base_controller.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class Users::BaseController < ApplicationController
-  before_action :load_user!
+  before_action :load_user!, :set_page_meta
 
   private
 
   def load_user!
     @user = User.find_by uid: params[:user_uid]
     redirect_to root_path if @user.blank?
+  end
+
+  def set_page_meta
+    @page_title = @user.name
+    @page_description = @user.bio
+    @page_image = @user.avatar
   end
 end
