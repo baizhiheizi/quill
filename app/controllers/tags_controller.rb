b79f9dc81fa6fc92f39c7445ffb59ad09d547c9f
an-lee <an.lee.work@gmail.com>
2021-12-29 21:25:23 +0800

    remove deprecated file

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -12,12 +12,4 @@ class TagsController < ApplicationController
       end
     end
   end
-
-  def show
-    tag = Tag.find_by id: params[:id]
-    return if tag.blank?
-
-    @page_title = "##{tag.name} 话题文章"
-    @page_description = "##{tag.name} 话题下有 #{tag.articles_count} 篇文章"
-  end
 end
