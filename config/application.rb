c2e55d995b168818a39f6127907846d61ecc1d9e
an-lee <an.lee.work@gmail.com>
2021-12-29 09:58:18 +0800

    upgrade rails 7.0

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,6 @@
-# frozen_string_literal: true
+require_relative "boot"
 
-require_relative 'boot'
-
-require 'rails/all'
-require 'pagy/extras/countless'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,10 +11,13 @@ module Prsdigg
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.time_zone = Settings.time_zone if Settings.time_zone.present?
     config.i18n.available_locales = Settings.available_locales || %i[en]
@@ -25,14 +25,5 @@ module Prsdigg
 
     # custom error pages
     config.exceptions_app = routes
-
-    # reference:
-    # https://stackoverflow.com/questions/49233769/is-there-a-way-to-prevent-safari-on-ios-from-clearing-the-cookies-for-a-website
-    # https://developer.mozilla.org/en-US/docs/Web/API/Document/cookie#Example_4_Reset_the_previous_cookie
-    # https://api.rubyonrails.org/v5.2.1/classes/ActionDispatch/Session/CookieStore.html
-    config.session_store :cookie_store, expire_after: 14.days, key: '_prsdigg_session'
-
-    # https://github.com/exAspArk/batch-loader#caching
-    config.middleware.use BatchLoader::Middleware
   end
 end
