29174ed090dbe506c503b0a4a1d76f97650bbeba
an-lee <an.lee.work@gmail.com>
2021-12-30 11:07:30 +0800

    remove depcreated js files

--- app/packs/apps/dashboard/pages/CommentsPage/CommentsPage.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function CommentsPage() {
-  return <div>建设中...</div>;
-}
--- app/packs/entrypoints/landing.js
+++
@@ -1,6 +0,0 @@
-require('@rails/ujs').start();
-require('@rails/activestorage').start();
-require('turbolinks').start();
-require('channels');
-
-import 'stylesheets/landing.css';
