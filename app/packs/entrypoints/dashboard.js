495d34c86793498740f191ff5ac6c6901e9cf60d
an-lee <an.lee.work@gmail.com>
2021-12-30 11:07:30 +0800

    remove depcreated js files

--- app/packs/entrypoints/dashboard.js
+++
@@ -1,8 +0,0 @@
-import ReactOnRails from 'react-on-rails';
-import DashboardApp from '../apps/dashboard/App';
-import '../stylesheets/dashboard.css';
-
-// This is how react_on_rails can see the HelloWorld in the browser.
-ReactOnRails.register({
-  DashboardApp,
-});
