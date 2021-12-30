2fa5247c61f6332148b9c8322c0b0383726a9b02
an-lee <an.lee.work@gmail.com>
2021-12-30 22:17:00 +0800

    add exception notifier

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,8 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.middleware.use ExceptionNotification::Rack, mixin_bot: {
+    recipient_id: Rails.application.credentials.dig(:admin, :uuid)
+  }
 end
