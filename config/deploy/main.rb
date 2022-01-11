6626024d4d9bc5aa8168110ea26187854d1624e9
an-lee <an.lee.work@gmail.com>
2022-01-11 09:09:39 +0800

    update ja deploy task

--- config/deploy/main.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-set :domain, 'prsdigg.com'
-set :deploy_to, '/data/www/prsdigg'
-set :repository, 'git@github.com:baizhiheizi/prsdigg.git'
-set :branch, 'build-main'
-set :user, 'ruby'
