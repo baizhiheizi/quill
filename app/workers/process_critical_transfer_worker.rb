eea8ca84beca8e1e3aed40f49a48621aa008f43d
an-lee <an.lee.work@gmail.com>
2022-04-18 18:25:12 +0800

    refactor transfer job retry interval

--- app/workers/process_critical_transfer_worker.rb
+++ app/workers/process_critical_transfer_worker.rb
@@ -4,6 +4,13 @@ class ProcessCriticalTransferWorker
   include Sidekiq::Worker
   sidekiq_options queue: :critical, retry: true
 
+  sidekiq_retry_in do |count, exception|
+    case exception
+    when MixinBot::InsufficientPoolError, MixinBot::PinError
+      SecureRandom.random_number(60) if count < 10
+    end
+  end
+
   def perform(trace_id)
     Transfer.find_by(trace_id: trace_id)&.process!
   end
--- app/workers/process_transfer_worker.rb
+++ app/workers/process_transfer_worker.rb
@@ -4,6 +4,13 @@ class ProcessTransferWorker
   include Sidekiq::Worker
   sidekiq_options queue: :low, retry: true
 
+  sidekiq_retry_in do |count, exception|
+    case exception
+    when MixinBot::InsufficientPoolError, MixinBot::PinError
+      SecureRandom.random_number(60) if count < 10
+    end
+  end
+
   def perform(trace_id)
     Transfer.find_by(trace_id: trace_id)&.process!
   end
