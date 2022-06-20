1fd543f419631424519bec408a4669f5c6c0aa6b
an-lee <an.lee.work@gmail.com>
2022-06-20 10:27:52 +0800

    throttle order distribution

--- app/workers/distribute_order_worker.rb
+++ app/workers/distribute_order_worker.rb
@@ -2,7 +2,13 @@
 
 class DistributeOrderWorker
   include Sidekiq::Worker
+  include Sidekiq::Throttled::Worker
+
   sidekiq_options queue: :default, retry: true
+
+  sidekiq_throttle(
+    concurrency: { limit: 1, key_suffix: ->(trace_id) { trace_id } }
+  )
 
   def perform(trace_id)
     order = Order.find_by trace_id: trace_id
