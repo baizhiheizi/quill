f996f2d34ed815562a5ff03ca2491ee4ac8e1ec8
an-lee <an.lee.work@gmail.com>
2022-06-20 08:24:22 +0800

    add loading when submit/redirect

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,27 @@
 import 'abortcontroller-polyfill/dist/polyfill-patch-fetch';
 import '@hotwired/turbo-rails';
 import './controllers';
+import { hideLoading, showLoading } from './utils';
+
+addEventListener('turbo:submit-start', ({ target }) => {
+  for (const field of target.elements) {
+    field.disabled = true;
+  }
+  showLoading();
+});
+
+addEventListener('turbo:submit-end', () => {
+  hideLoading();
+});
+
+addEventListener('turbo:click', () => {
+  showLoading();
+});
+
+addEventListener('turbo:before-render', () => {
+  hideLoading();
+});
+
+addEventListener('turbo:frame-render', () => {
+  hideLoading();
+});
