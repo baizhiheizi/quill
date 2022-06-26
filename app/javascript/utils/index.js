57937b324816c81c8e94b349225fbb4b2c45e9b0
an-lee <an.lee.work@gmail.com>
2022-06-26 22:49:14 +0800

    add mvm register contract

--- app/javascript/utils/index.js
+++ app/javascript/utils/index.js
@@ -1,3 +1,7 @@
+export * from 'utils/abis';
+export * from 'utils/register';
+export * from 'utils/mvm';
+
 export const showLoading = () => {
   document.querySelector('#loading-toast').classList.remove('hidden');
 };
