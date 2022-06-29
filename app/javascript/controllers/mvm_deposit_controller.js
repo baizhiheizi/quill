65ce063f80c5da9c1c84415fc483b891ab5f374e
an-lee <an.lee.work@gmail.com>
2022-06-29 09:44:21 +0800

    check mvm  balance before pay

--- app/javascript/controllers/mvm_deposit_controller.js
+++ app/javascript/controllers/mvm_deposit_controller.js
@@ -1,9 +1,12 @@
 import { Controller } from '@hotwired/stimulus';
 import { get } from '@rails/request.js';
+import { RegistryContract, notify, showLoading, hideLoading } from 'utils';
 
 export default class extends Controller {
   static values = {
     assetId: String,
+    assetSymbol: String,
+    assetIconUrl: String,
     identifier: String,
   };
 
@@ -20,16 +23,64 @@ export default class extends Controller {
 
       if (identifier === this.identifierValue) {
         this.assetIdValue = event.detail.assetId;
-        this.currencyIconTarget.src = event.detail.iconUrl;
+        this.assetSymbolValue = event.detail.symbol;
+        this.assetIconUrlValue = event.detail.iconUrl;
+
+        this.currencyIconTargets.forEach((icon) => {
+          icon.src = event.detail.iconUrl;
+        });
         this.currencyChainIconTarget.src = event.detail.chainIconUrl;
         this.currencySymbolTarget.innerText = event.detail.symbol;
       }
     });
   }
 
+  async addToken() {
+    if (!this.assetIdValue) return;
+
+    showLoading();
+    try {
+      const registry = new RegistryContract();
+      const assetContractAddress = await registry.fetchAssetContract(
+        this.assetIdValue,
+      );
+
+      console.log(assetContractAddress);
+
+      if (!assetContractAddress || !parseInt(assetContractAddress)) {
+        notify(`Desposit some ${this.assetSymbolValue} first`, 'warning');
+        return;
+      }
+
+      await ethereum
+        .request({
+          method: 'wallet_watchAsset',
+          params: {
+            type: 'ERC20',
+            options: {
+              address: assetContractAddress,
+              symbol: this.assetSymbolValue,
+              decimals: 8,
+              image: this.assetIconUrlValue,
+            },
+          },
+        })
+        .then((success) => {
+          if (success) {
+            notify(`Successfully add ${this.assetSymbolValue}`, 'success');
+          } else {
+            notify(`Failed to add ${this.assetSymbolValue}`, 'warning');
+          }
+        });
+    } catch (error) {
+      notify(error, 'danger');
+    }
+
+    hideLoading();
+  }
+
   selectCurrency(event) {
     const asset_id = event.target.value;
-    console.log(asset_id);
     get(`/dashboard/destination?asset_id=${asset_id}`, {
       contentType: 'application/json',
       responseKind: 'turbo-stream',
