f44c0dd193d9016ce48ec12ce4615aaf7bb505df
an-lee <an.lee.work@gmail.com>
2022-06-29 15:22:57 +0800

    fix some js

--- app/javascript/controllers/metamask_controller.js
+++ app/javascript/controllers/metamask_controller.js
@@ -15,30 +15,118 @@ const XIN_ASSET_ID = 'c94ac88f-4671-3976-b60a-09064f1811e8';
 export default class extends Controller {
   static targets = ['loginButton', 'waiting'];
 
-  login(event) {
+  async login(event) {
     event.preventDefault();
 
-    this.requestLogin();
+    const { account, web3 } = await ensureEthAccountExist();
+    if (!account) return;
+
+    this.lockButton();
+    try {
+      const nounce = await this.getNounce(account);
+      const signature = await web3.eth.personal.sign(
+        JSON.stringify(nounce),
+        account,
+      );
+
+      Turbo.visit(
+        `/auth/mvm/callback?signature=${signature}&public_key=${account}&return_to=${encodeURIComponent(
+          location.href,
+        )}`,
+      );
+    } catch (error) {
+      notify(error.message, 'danger');
+      this.unlockButton();
+    }
   }
 
-  async requestLogin() {
+  async pay(event) {
+    event.preventDefault();
+
+    try {
+      const { assetId, symbol, amount, opponentId, memo, contract } =
+        event.params;
+      if (!contract) return;
+
+      const { extra } = await this.fetchExtra(opponentId, memo);
+
+      if (assetId === XIN_ASSET_ID) {
+        await this.payXIN({ symbol, amount, contract, extra });
+      } else {
+        await this.payERC20({ assetId, symbol, amount, contract, extra });
+      }
+    } catch (error) {
+      notify(error.message, 'danger');
+      this.unlockButton();
+    }
+  }
+
+  async payERC20(params) {
+    const { assetId, symbol, amount, contract, extra } = params;
     const { account, web3 } = await ensureEthAccountExist();
 
-    const nounce = await this.getNounce();
-    const signature = await web3.eth.personal.sign(
-      JSON.stringify(nounce),
-      account,
-    );
+    this.lockButton();
 
-    Turbo.visit(
-      `/auth/mvm/callback?signature=${signature}&public_key=${account}&return_to=${encodeURIComponent(
-        location.href,
-      )}`,
-    );
+    const registry = new RegistryContract();
+    const assetContractAddress = await registry.fetchAssetContract(assetId);
+    if (!assetContractAddress || !parseInt(assetContractAddress)) {
+      notify('Desposit some asset first', 'warning');
+      this.unlockButton();
+      return;
+    }
+
+    let IERC20 = new web3.eth.Contract(ERC20ABI, assetContractAddress);
+
+    let payAmount = parseInt(parseFloat(amount) * 1e8);
+    const balance = await IERC20.methods.balanceOf(account).call();
+    if (balance < payAmount) {
+      notify('Insufficient balance', 'warning');
+      this.unlockButton();
+      return;
+    }
+
+    IERC20.methods
+      .transferWithExtra(contract, payAmount, `0x${extra}`)
+      .send({ from: account })
+      .on('sent', () => {
+        notify(`Invoking MetaMask to pay ${amount} ${symbol}`, 'info');
+      })
+      .on('transactionHash', () => {
+        this.unlockButton();
+        notify('Successfully paid', 'success');
+        this.element.outerHTML = this.waitingTarget.innerHTML;
+      })
+      .on('error', (error) => {
+        this.unlockButton();
+        notify(error.message, 'danger');
+      });
   }
 
-  async getNounce() {
-    const { account } = await ensureEthAccountExist();
+  async payXIN(params) {
+    const { symbol, amount, contract, extra } = params;
+    const { account, web3 } = await ensureEthAccountExist();
+    const BridgeContract = new web3.eth.Contract(BridgeABI, BridgeAddress);
+
+    this.lockButton();
+
+    BridgeContract.methods
+      .release(contract, `0x${extra}`)
+      .send({ from: account, value: parseInt(parseFloat(amount) * 1e18) })
+      .on('sent', () => {
+        notify(`Invoking MetaMask to pay ${amount} ${symbol}`, 'info');
+      })
+      .on('transactionHash', () => {
+        notify('Successfully paid', 'success');
+        this.unlockButton();
+        this.element.outerHTML = this.waitingTarget.innerHTML;
+      })
+      .on('error', (error) => {
+        this.unlockButton();
+        notify(error.message, 'danger');
+      });
+  }
+
+  async getNounce(account) {
     const res = await post('/nounce', {
       body: {
         public_key: account,
@@ -49,14 +137,7 @@ export default class extends Controller {
     return await res.json;
   }
 
-  async pay(event) {
-    event.preventDefault();
-
-    const { assetId, symbol, amount, opponentId, memo, contract } =
-      event.params;
-    if (!contract) return;
-
-    showLoading();
+  async fetchExtra(opponentId, memo) {
     const res = await post('/mvm/extras', {
       body: {
         receivers: [opponentId],
@@ -64,73 +145,16 @@ export default class extends Controller {
         extra: memo,
       },
     });
-    const { extra } = await res.json;
-
-    if (assetId === XIN_ASSET_ID) {
-      await this.payXIN({ symbol, amount, contract, extra });
-    } else {
-      await this.payERC20({ assetId, symbol, amount, contract, extra });
-    }
+    return await res.json;
   }
 
-  async payERC20(params) {
-    const { assetId, symbol, amount, contract, extra } = params;
-    const { account, web3 } = await ensureEthAccountExist();
-
-    const registry = new RegistryContract();
-    const assetContractAddress = await registry.fetchAssetContract(assetId);
-    if (!assetContractAddress || !parseInt(assetContractAddress)) {
-      notify('Desposit some asset first', 'warning');
-      return;
-    }
-
-    let IERC20 = new web3.eth.Contract(ERC20ABI, assetContractAddress);
-
-    let payAmount = parseInt(parseFloat(amount) * 1e8);
-    const balance = await IERC20.methods.balanceOf(account).call();
-    if (balance < payAmount) {
-      notify('Insufficient balance', 'warning');
-      return;
-    }
-
-    IERC20.methods
-      .transferWithExtra(contract, payAmount, `0x${extra}`)
-      .send({ from: account })
-      .on('sent', () => {
-        notify(`Invoking MetaMask to pay ${amount} ${symbol}`, 'info');
-        this.element.setAttribute('disabled', true);
-      })
-      .on('transactionHash', () => {
-        notify('Already paid', 'success');
-        this.element.outerHTML = this.waitingTarget.innerHTML;
-      })
-      .on('error', (error) => {
-        this.element.removeAttribute('disabled');
-        notify(error.message, 'danger');
-      })
-      .finally(hideLoading);
+  lockButton() {
+    this.element.setAttribute('disabled', true);
+    showLoading();
   }
 
-  async payXIN(params) {
-    const { symbol, amount, contract, extra } = params;
-    const { account, web3 } = await ensureEthAccountExist();
-
-    const BridgeContract = new web3.eth.Contract(BridgeABI, BridgeAddress);
-    BridgeContract.methods
-      .release(contract, `0x${extra}`)
-      .send({ from: account, value: parseInt(parseFloat(amount) * 1e18) })
-      .on('sent', () => {
-        notify(`Invoking MetaMask to pay ${amount} ${symbol}`, 'info');
-        this.element.setAttribute('disabled', true);
-      })
-      .on('transactionHash', () => {
-        notify('Already paid', 'success');
-        this.element.outerHTML = this.waitingTarget.innerHTML;
-      })
-      .on('error', (error) => {
-        this.element.removeAttribute('disabled');
-        notify(error.message, 'danger');
-      })
-      .finally(hideLoading);
+  unlockButton() {
+    this.element.removeAttribute('disabled');
+    hideLoading();
   }
 }
