fc89d87cbaec86f7445240349623e8b101bb3f75
an-lee <an.lee.work@gmail.com>
2022-06-29 14:10:07 +0800

    refactor

--- app/javascript/controllers/metamask_controller.js
+++ app/javascript/controllers/metamask_controller.js
@@ -15,37 +15,33 @@ const XIN_ASSET_ID = 'c94ac88f-4671-3976-b60a-09064f1811e8';
 export default class extends Controller {
   static targets = ['loginButton', 'waiting'];
 
-  async connect() {}
-
-  async login(event) {
+  login(event) {
     event.preventDefault();
 
-    const { account, web3 } = await ensureEthAccountExist();
-    this.account = account;
-    this.web3 = web3;
-
-    if (!this.account) return;
-    await this.requestLogin();
+    this.requestLogin();
   }
 
   async requestLogin() {
+    const { account, web3 } = await ensureEthAccountExist();
+
     const nounce = await this.getNounce();
-    const signature = await this.web3.eth.personal.sign(
+    const signature = await web3.eth.personal.sign(
       JSON.stringify(nounce),
-      this.account,
+      account,
     );
 
     Turbo.visit(
-      `/auth/mvm/callback?signature=${signature}&public_key=${
-        this.account
-      }&return_to=${encodeURIComponent(location.href)}`,
+      `/auth/mvm/callback?signature=${signature}&public_key=${account}&return_to=${encodeURIComponent(
+        location.href,
+      )}`,
     );
   }
 
   async getNounce() {
+    const { account } = await ensureEthAccountExist();
     const res = await post('/nounce', {
       body: {
-        public_key: this.account,
+        public_key: account,
       },
       contentType: 'application/json',
     });
@@ -58,9 +54,7 @@ export default class extends Controller {
 
     const { assetId, symbol, amount, opponentId, memo, contract } =
       event.params;
-
     if (!contract) return;
-    if (!this.account) return;
 
     showLoading();
     const res = await post('/mvm/extras', {
@@ -73,15 +67,15 @@ export default class extends Controller {
     const { extra } = await res.json;
 
     if (assetId === XIN_ASSET_ID) {
-      this.payXIN({ symbol, amount, contract, extra });
+      await this.payXIN({ symbol, amount, contract, extra });
     } else {
-      this.payERC20({ assetId, symbol, amount, contract, extra });
+      await this.payERC20({ assetId, symbol, amount, contract, extra });
     }
-    hideLoading();
   }
 
   async payERC20(params) {
     const { assetId, symbol, amount, contract, extra } = params;
+    const { account, web3 } = await ensureEthAccountExist();
 
     const registry = new RegistryContract();
     const assetContractAddress = await registry.fetchAssetContract(assetId);
@@ -90,11 +84,10 @@ export default class extends Controller {
       return;
     }
 
-    let IERC20 = new this.web3.eth.Contract(ERC20ABI, assetContractAddress);
+    let IERC20 = new web3.eth.Contract(ERC20ABI, assetContractAddress);
 
     let payAmount = parseInt(parseFloat(amount) * 1e8);
-    const balance = await IERC20.methods.balanceOf(this.account).call();
-    console.log(balance);
+    const balance = await IERC20.methods.balanceOf(account).call();
     if (balance < payAmount) {
       notify('Insufficient balance', 'warning');
       return;
@@ -102,7 +95,7 @@ export default class extends Controller {
 
     IERC20.methods
       .transferWithExtra(contract, payAmount, `0x${extra}`)
-      .send({ from: this.account })
+      .send({ from: account })
       .on('sent', () => {
         notify(`Invoking MetaMask to pay ${amount} ${symbol}`, 'info');
         this.element.setAttribute('disabled', true);
@@ -114,16 +107,18 @@ export default class extends Controller {
       .on('error', (error) => {
         this.element.removeAttribute('disabled');
         notify(error.message, 'danger');
-      });
+      })
+      .finally(hideLoading);
   }
 
   async payXIN(params) {
     const { symbol, amount, contract, extra } = params;
+    const { account, web3 } = await ensureEthAccountExist();
 
-    const BridgeContract = new this.web3.eth.Contract(BridgeABI, BridgeAddress);
+    const BridgeContract = new web3.eth.Contract(BridgeABI, BridgeAddress);
     BridgeContract.methods
       .release(contract, `0x${extra}`)
-      .send({ from: this.account, value: parseInt(parseFloat(amount) * 1e18) })
+      .send({ from: account, value: parseInt(parseFloat(amount) * 1e18) })
       .on('sent', () => {
         notify(`Invoking MetaMask to pay ${amount} ${symbol}`, 'info');
         this.element.setAttribute('disabled', true);
@@ -135,6 +130,7 @@ export default class extends Controller {
       .on('error', (error) => {
         this.element.removeAttribute('disabled');
         notify(error.message, 'danger');
-      });
+      })
+      .finally(hideLoading);
   }
 }
--- app/javascript/utils/mvm.js
+++ app/javascript/utils/mvm.js
@@ -1,5 +1,6 @@
 import detectEthereumProvider from '@metamask/detect-provider';
 import 'web3';
+import { hideLoading } from 'utils/toast';
 
 export const provider = await detectEthereumProvider();
 export const MVM_CHAIN_ID = '0x120c7';
@@ -19,20 +20,22 @@ export async function ensureEthAccountExist() {
 }
 
 export async function addMvmChain() {
-  ethereum.request({
-    method: 'wallet_addEthereumChain',
-    params: [
-      {
-        chainId: MVM_CHAIN_ID,
-        chainName: 'Mixin Virtual Machine',
-        nativeCurrency: {
-          name: 'Mixin',
-          symbol: 'XIN',
-          decimals: 18,
+  ethereum
+    .request({
+      method: 'wallet_addEthereumChain',
+      params: [
+        {
+          chainId: MVM_CHAIN_ID,
+          chainName: 'Mixin Virtual Machine',
+          nativeCurrency: {
+            name: 'Mixin',
+            symbol: 'XIN',
+            decimals: 18,
+          },
+          rpcUrls: ['https://geth.mvm.dev'],
+          blockExplorerUrls: ['https://scan.mvm.dev/'],
         },
-        rpcUrls: ['https://geth.mvm.dev'],
-        blockExplorerUrls: ['https://scan.mvm.dev/'],
-      },
-    ],
-  });
+      ],
+    })
+    .finally(hideLoading);
 }
