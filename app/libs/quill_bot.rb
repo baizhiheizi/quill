b1881294622d41fde7f82792b448057124b8eb4a
an-lee <an.lee.work@gmail.com>
2022-08-03 16:33:33 +0800

    rename config

--- app/libs/quill_bot.rb
+++ app/libs/quill_bot.rb
@@ -3,11 +3,11 @@
 module QuillBot
   def self.api
     @api ||= MixinBot::API.new(
-      client_id: Rails.application.credentials.dig(:prsdigg_bot, :client_id),
-      client_secret: Rails.application.credentials.dig(:prsdigg_bot, :client_secret),
-      session_id: Rails.application.credentials.dig(:prsdigg_bot, :session_id),
-      pin_token: Rails.application.credentials.dig(:prsdigg_bot, :pin_token),
-      private_key: Rails.application.credentials.dig(:prsdigg_bot, :private_key)
+      client_id: Rails.application.credentials.dig(:quill_bot, :client_id),
+      client_secret: Rails.application.credentials.dig(:quill_bot, :client_secret),
+      session_id: Rails.application.credentials.dig(:quill_bot, :session_id),
+      pin_token: Rails.application.credentials.dig(:quill_bot, :pin_token),
+      private_key: Rails.application.credentials.dig(:quill_bot, :private_key)
     )
   end
 end
