538cb372bbe38564f8eb9eff83748d3381bed473
an-lee <an.lee.work@gmail.com>
2022-08-14 20:51:45 +0800

    remove notification for pg connection lost

--- app/models/mixin_network_snapshot.rb
+++ app/models/mixin_network_snapshot.rb
@@ -78,7 +78,6 @@ class MixinNetworkSnapshot < ApplicationRecord
       sleep POLLING_INTERVAL * 10
     rescue ActiveRecord::StatementInvalid => e
       p e.inspect
-      ExceptionNotifier.notify_exception e
       connection.reconnect!
     rescue StandardError => e
       p "#{e.inspect}\n#{e.backtrace.join("\n")}"
