b839e2600685f90497807a9c2ad9ae890a2c03f3
an-lee <an.lee.work@gmail.com>
2022-08-16 22:14:37 +0800

    remove unused route

--- app/controllers/mvm/extras_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class MVM::ExtrasController < MVM::BaseController
-  def create
-    res = MVM.api.extra receivers: params[:receivers], threshold: params[:threshold], extra: params[:extra]
-
-    render json: res
-  end
-end
--- config/routes/mvm.rb
+++ config/routes/mvm.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 namespace :mvm do
-  resources :extras, only: :create
   resource :faucet, only: :create
   resource :swap, only: :create
 end
