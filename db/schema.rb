e991db0c3dcbe023c2ad65e60aeeebefcd977463
an-lee <an.lee.work@gmail.com>
2022-08-22 08:40:09 +0800

    fix migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_19_093249) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_22_003643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "pgcrypto"
