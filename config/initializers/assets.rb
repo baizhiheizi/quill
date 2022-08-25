eb2a2e381a6055eb3994d82c32e9527005339093
an-lee <an.lee.work@gmail.com>
2022-08-25 11:47:02 +0800

    fix local render slow issue

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,13 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
-Rails.application.config.assets.paths += %w[node_modules]
+Rails.application.config.assets.paths +=
+  %w[
+    node_modules/highlight.js/styles
+    node_modules/photoswipe/dist
+    node_modules/tom-select/dist/css
+    node_modules/easymde/dist
+  ]
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
