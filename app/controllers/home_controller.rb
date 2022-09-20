3c9f46832f2e3b566138271049e322c8e9aad6af
an-lee <an.lee.work@gmail.com>
2022-09-20 11:03:03 +0800

    article show style

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,10 +12,22 @@ class HomeController < ApplicationController
   end
 
   def hot_tags
-    @hot_tags = Tag.hot.where(locale: current_locale.to_s.split('-').first).limit(50).sample(5)
+    @hot_tags =
+      Tag
+      .hot
+      .where(locale: current_locale.to_s.split('-').first)
+      .limit(50)
+      .sample(5)
   end
 
   def active_authors
-    @users = User.only_validated.active.where.not(id: current_user&.block_user_ids).where(locale: current_locale).limit(20).sample(5)
+    @users =
+      User
+      .active
+      .where.not(id: current_user&.block_user_ids)
+      .where.not(id: current_user&.id)
+      .where(locale: current_locale)
+      .limit(20)
+      .sample(5)
   end
 end
