515b6fc6e34632fbb919fc7b43ef853a30c7b3f5
an-lee <an.lee.work@gmail.com>
2022-09-21 10:25:11 +0800

    update article list component

--- app/components/articles/list_component.rb
+++ app/components/articles/list_component.rb
@@ -1,10 +1,11 @@
 # frozen_string_literal: true
 
 class Articles::ListComponent < ApplicationComponent
-  def initialize(articles: [], pagy: nil)
+  def initialize(articles: [], pagy: nil, pagy_id: 'articles_pagination')
     super
 
     @articles = articles.presence || ArticleSearchService.call
     @pagy = pagy
+    @pagy_id = pagy_id
   end
 end
