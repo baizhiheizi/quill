ef6a815010cd01a9351e4847b33e666fcc70b112
an-lee <an.lee.work@gmail.com>
2022-09-27 17:00:29 +0800

    active page

--- app/controllers/dashboard/notifications_controller.rb
+++ app/controllers/dashboard/notifications_controller.rb
@@ -3,6 +3,7 @@
 class Dashboard::NotificationsController < Dashboard::BaseController
   def index
     @pagy, @notifications = pagy current_user.notifications.order(created_at: :desc)
+    @active_page = 'notification'
   end
 
   def show
