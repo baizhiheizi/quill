6f6e6a563678db191c0d9b5b4975ab80202bcc45
an-lee <an.lee.work@gmail.com>
2022-10-26 18:13:25 +0800

    may generate artile poster

--- app/controllers/grover/base_controller.rb
+++ app/controllers/grover/base_controller.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
 class Grover::BaseController < ApplicationController
-  layout false
+  before_action :authenticate!
+
+  layout 'grover'
+
+  private
+
+  def authenticate!
+    raise unless Rails.application.credentials.dig(:grover, :token) == params[:token]
+  end
 end
