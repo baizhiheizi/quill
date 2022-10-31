bd8bceb3283b40615501c40332c85c6dc739c40a
an-lee <an.lee.work@gmail.com>
2022-10-31 21:35:01 +0800

    fix article default locale

--- app/models/article.rb
+++ app/models/article.rb
@@ -400,7 +400,7 @@ class Article < ApplicationRecord
         locales.reject(&->(l) { l == 'en' }).last
       end
     else
-      author.locale
+      author.locale.split('-').first
     end
   end
 
