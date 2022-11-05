24080898a3a09ce3b3b9dadd2164fd4c37b273a9
an-lee <an.lee.work@gmail.com>
2022-11-05 10:48:54 +0800

    fix typo

--- db/migrate/20221105021058_edit_ar_tx_index.rb
+++ db/migrate/20221105021058_edit_ar_tx_index.rb
@@ -1,7 +1,7 @@
 class EditArTxIndex < ActiveRecord::Migration[7.0]
   def change
     remove_column :arweave_transactions, :hash, if_exists: true
-    add_column :arweave_transactions, :digest, if_exists: true
+    add_column :arweave_transactions, :digest, :string, if_exists: true
     remove_index :arweave_transactions, %i[article_uuid digest], name: :index_arweave_transactions_on_article_uuid_and_digest, unique: true, if_exists: true
 
     add_index :arweave_transactions, %i[article_uuid owner_id digest], name: :index_ar_tx_on_article_uuid_and_owner_id_and_digest, unique: true
