771ce2d76a8e2f868193beee0fda307b5bbf4011
an-lee <an.lee.work@gmail.com>
2022-11-09 09:39:29 +0800

    tune floating button hidden when scroll

--- app/javascript/controllers/floating_controller.js
+++ app/javascript/controllers/floating_controller.js
@@ -3,15 +3,19 @@ import { debounce } from 'underscore';
 
 export default class extends Controller {
   connect() {
+    this.listenToScroll();
+  }
+
+  listenToScroll() {
     let scrolling;
     document.addEventListener('scroll', () => {
       clearTimeout(scrolling);
 
-      debounce(this.element.classList.add('hidden'), 500);
+      debounce(this.element.classList.add('hidden'), 100);
 
       scrolling = setTimeout(() => {
         this.element.classList.remove('hidden');
-      }, 500);
+      }, 100);
     });
   }
 }
