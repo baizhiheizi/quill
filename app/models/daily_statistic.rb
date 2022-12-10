e8244d3dd1c1b6657e4b55ed67d0cd67829c4134
an-lee <an.lee.work@gmail.com>
2022-12-10 12:10:28 +0800

    refactor statistic

--- app/models/daily_statistic.rb
+++ app/models/daily_statistic.rb
@@ -13,12 +13,14 @@
 #
 
 class DailyStatistic < Statistic
-  store :data, accessors: %i[
+  store_accessor :data, %i[
     new_users_count
     paid_users_count
     new_payments_count
     new_payers_count
     new_articles_count
+    author_revenue_total_in_usd
+    reader_revenue_total_in_usd
   ]
 
   before_validation :setup_attributes, on: :create
@@ -38,14 +40,16 @@ class DailyStatistic < Statistic
   def data_attributes(date)
     {
       new_users_count: User.where(created_at: date.beginning_of_day...date.end_of_day).count,
-      paid_users_count: Order.completed.where(order_type: %i[buy_article reward_article], created_at: ...date.end_of_day).pluck(:buyer_id).uniq.count,
-      new_payments_count: Order.completed.where(order_type: %i[buy_article reward_article], created_at: date.beginning_of_day...date.end_of_day).count,
-      new_payers_count: Order.completed.where(order_type: %i[buy_article reward_article], created_at: date.beginning_of_day...date.end_of_day).pluck(:buyer_id).uniq.count,
-      new_articles_count: Article.where(published_at: date.beginning_of_day...date.end_of_day).count
+      paid_users_count: Order.completed.where(order_type: %i[buy_article reward_article buy_collection], created_at: ...date.end_of_day).pluck(:buyer_id).uniq.count,
+      new_payments_count: Order.completed.where(order_type: %i[buy_article reward_article buy_collection], created_at: date.beginning_of_day...date.end_of_day).count,
+      new_payers_count: Order.completed.where(order_type: %i[buy_article reward_article buy_collection], created_at: date.beginning_of_day...date.end_of_day).pluck(:buyer_id).uniq.count,
+      new_articles_count: Article.where(published_at: date.beginning_of_day...date.end_of_day).count,
+      author_revenue_total_in_usd: Transfer.joins(:currency).where(transfer_type: :author_revenue, created_at: date.beginning_of_day...date.end_of_day).sum('amount * currencies.price_usd').to_f.round(4),
+      reader_revenue_total_in_usd: Transfer.joins(:currency).where(transfer_type: :reader_revenue, created_at: date.beginning_of_day...date.end_of_day).sum('amount * currencies.price_usd').to_f.round(4)
     }
   end
 
   def setup_attributes
-    assign_attributes data_attributes(datetime)
+    assign_attributes data: data_attributes(datetime)
   end
 end
