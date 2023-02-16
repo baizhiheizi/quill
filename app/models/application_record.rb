bda0a03a0bb6ab25ff07670d2b5075eee73e8761
an-lee <an.lee.work@gmail.com>
2023-02-16 12:39:41 +0800

    fix ransack breaking change

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,4 +4,8 @@ class ApplicationRecord < ActiveRecord::Base
   include Rails.application.routes.url_helpers
 
   primary_abstract_class
+
+  def self.ransackable_attributes(auth_object = nil)
+    column_names + _ransackers.keys
+  end
 end
