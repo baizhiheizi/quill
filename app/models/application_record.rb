b9bdbc6822c552e1fa8811bed4a6fb61ad77c6f0
an-lee <an.lee.work@gmail.com>
2023-02-16 12:58:29 +0800

    fix ransack

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -8,4 +8,16 @@ class ApplicationRecord < ActiveRecord::Base
   def self.ransackable_attributes(auth_object = nil)
     column_names + _ransackers.keys
   end
+
+  def self.ransackable_associations(auth_object = nil)
+    reflect_on_all_associations.map { |a| a.name.to_s }
+  end
+
+  def self.ransortable_attributes(auth_object = nil)
+    ransackable_attributes(auth_object)
+  end
+
+  def self.ransackable_scopes(auth_object = nil)
+    []
+  end
 end
