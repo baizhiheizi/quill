9aff2a61c26e5bb124a7b26cc4236a9055bfcc6a
an-lee <an.lee.work@gmail.com>
2023-04-01 17:31:17 +0800

    use route id when create 4swap action

--- app/models/concerns/pre_orders/swappable.rb
+++ app/models/concerns/pre_orders/swappable.rb
@@ -28,53 +28,60 @@ module PreOrders::Swappable
     Addressable::URI.new(
       scheme: 'mixin',
       host: 'codes',
-      path: foxswap_pay_code_id(pay_asset_id)
+      path: fswap_pay_code_id(pay_asset_id)
     ).to_s
   end
 
-  def foxswap_pay_code_id(pay_asset_id)
+  def fswap_pay_code_id(pay_asset_id)
     return if pay_asset_id == asset_id
+
+    route = fswap_route pay_asset_id
+    return if route.blank?
 
     QuillBot.api.create_payment(
       asset_id: pay_asset_id,
-      memo: fswap_mtg_memo,
-      amount: pay_amount(pay_asset_id),
+      memo: fswap_mtg_memo(route[:routes]),
+      amount: route[:funds],
       receivers: Settings.foxswap.mtg_members,
       threshold: Settings.foxswap.mtg_threshold
     )['code_id']
   end
 
-  def fswap_mtg_memo
+  def fswap_mtg_memo(route_id = nil)
     r = PandoBot::Lake.api.actions(
       user_id: payee_id,
       follow_id: follow_id,
       asset_id: asset_id,
+      route_id: route_id,
       minimum_fill: order_type == 'reward_article' ? nil : amount
     )
 
     r['data']['action']
   end
 
-  def pay_amount(pay_asset_id = nil)
-    @pay_amount ||=
-      case pay_asset_id
-      when asset_id
-        amount
-      else
-        begin
-          pairs = Rails.cache.fetch 'pando_lake_routes', expires_in: 5.seconds do
-            PandoBot::Lake.api.pairs['data']['pairs']
-          end
+  def pay_amount(pay_asset_id = asset_id)
+    if pay_asset_id == asset_id
+      amount
+    else
+      fswap_route(pay_asset_id)&.[](:funds)
+    end
+  end
 
-          routes ||= PandoBot::Lake::PairRoutes.new pairs
-          routes.pre_order(
-            input_asset: pay_asset_id,
-            output_asset: asset_id,
-            output_amount: (amount * 1.001).ceil(8).to_f
-          )[:funds]
-        rescue StandardError
-          nil
+  def fswap_route(pay_asset_id = nil)
+    @fswap_route ||=
+      begin
+        pairs = Rails.cache.fetch 'pando_lake_routes', expires_in: 5.seconds do
+          PandoBot::Lake.api.pairs['data']['pairs']
         end
+
+        routes ||= PandoBot::Lake::PairRoutes.new pairs
+        routes.pre_order(
+          input_asset: pay_asset_id,
+          output_asset: asset_id,
+          output_amount: (amount * 1.001).ceil(8).to_f
+        )
+      rescue StandardError
+        nil
       end
   end
 end
