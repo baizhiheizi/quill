d907dafad2af6aa3641bb92c9d9be2b84dca44ac
an-lee <an.lee.work@gmail.com>
2024-06-14 20:58:09 +0800

    add report

--- app/jobs/daily_statistics/generate_job.rb
+++ app/jobs/daily_statistics/generate_job.rb
@@ -6,5 +6,15 @@ class DailyStatistics::GenerateJob < ApplicationJob
 
   def perform
     DailyStatistic.generate date: Time.current.yesterday
+
+    conversation_id = Rails.application.credentials.dig(:mixin_groups, :operation)
+    return if conversation_id.blank?
+
+    report = QuillBot.api.plain_post(
+      conversation_id:,
+      data: QuillBot.generate_app_report
+    )
+
+    MixinMessages::SendJob.perform_later report
   end
 end
--- app/libs/quill_bot.rb
+++ app/libs/quill_bot.rb
@@ -7,4 +7,34 @@ module QuillBot
     Rails.logger.error e
     nil
   end
+
+  def self.generate_app_report
+    <<~TEXT
+      ## Quill Report #{Time.current.to_date}
+
+      ### Last 24 Hours
+      - New Users: #{User.where(created_at: 24.hours.ago...).count}
+      - Articles: #{Article.where(published_at: 24.hours.ago...).count}
+      - Orders: #{Order.completed.where(created_at: 24.hours.ago...).count}
+      - Volume: $#{Order.completed.where(updated_at: 24.hours.ago...).sum(:value_usd).round(4)}
+
+      ### Last 7 days
+      - New Users: #{User.where(created_at: 7.days.ago...).count}
+      - Articles: #{Article.where(published_at: 7.days.ago...).count}
+      - Orders: #{Order.completed.where(created_at: 7.days.ago...).count}
+      - Volume: $#{Order.completed.where(updated_at: 7.days.ago...).sum(:value_usd).round(4)}
+
+      ### Last 30 days
+      - New Users: #{User.where(created_at: 30.days.ago...).count}
+      - Articles: #{Article.where(published_at: 30.days.ago...).count}
+      - Orders: #{Order.completed.where(created_at: 30.days.ago...).count}
+      - Volume: $#{Order.completed.where(updated_at: 30.days.ago...).sum(:value_usd).round(4)}
+
+      ### Total
+      - Users: #{User.count}
+      - Articles: #{Article.only_published.count}
+      - Orders: #{Order.completed.count}
+      - Volume: $#{Order.completed.sum(:value_usd).round(4)}
+    TEXT
+  end
 end
